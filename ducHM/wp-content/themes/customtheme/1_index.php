d151cfc09561eaf11d43308eee21205abb21667c
Ha Duc <haducvcvb@gmail.com>
2023-06-16 08:06:09 +0700

    saved code from 16/06/23

---
+++ ducHM/wp-content/themes/customtheme/1_index.php
@@ -0,0 +1,30 @@
+<!-- Sorting -->
+<?php get_header(); ?>
+
+<div class="container">
+    <h4 class="lesson-title">WP Custom - Ordering</h4>
+    <?php 
+        $arg = array(
+            'post_type'=> 'post',
+            'posts_per_page' => 20, //limited posts
+
+            // Start: order, orderby
+            'orderby' => 'meta_value', // 'title', 'id', 'rand', 'comment_count', 'date'...
+            // 'order'=> 'ASC', // ASC, DESC
+        );
+
+        $query = new WP_Query($arg);
+        while($query -> have_posts()) : $query -> the_post();
+    ?>
+        <!-- Add stuff here -->
+        <h5><?php 
+        // the_title();
+        ?></h5>
+
+        <ul>
+            <li><?php the_title(); ?></li>
+        </ul>
+    <?php endwhile; wp_reset_query(); ?>
+</div>
+
+<?php get_footer(); ?>
--- ducHM/wp-content/themes/customtheme/index.php
+++ ducHM/wp-content/themes/customtheme/index.php
@@ -1,30 +1,9 @@
-<!-- Sorting -->
+<!-- Basics -->
 <?php get_header(); ?>
 
 <div class="container">
-    <h4 class="lesson-title">WP Custom - Ordering</h4>
-    <?php 
-        $arg = array(
-            'post_type'=> 'post',
-            'posts_per_page' => 20, //limited posts
-
-            // Start: order, orderby
-            'orderby' => 'rand', // 'title', 'id', 'rand', 'comment_count', 'date'...
-            // 'order'=> 'ASC', // ASC, DESC
-        );
-
-        $query = new WP_Query($arg);
-        while($query -> have_posts()) : $query -> the_post();
-    ?>
-        <!-- Add stuff here -->
-        <h5><?php 
-        // the_title();
-        ?></h5>
-
-        <ul>
-            <li><?php the_title(); ?></li>
-        </ul>
-    <?php endwhile; wp_reset_query(); ?>
+    <h4 class="lesson-title">WP Custom - Basics</h4>
+    loo
 </div>
 
 <?php get_footer(); ?>
